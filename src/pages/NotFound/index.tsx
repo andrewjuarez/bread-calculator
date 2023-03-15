8fd8c1d5d2ceaa3b11edcbc824f9dc596281b557
andrewjuarez <a777juarez@gmail.com>
2023-03-14 23:24:14 -0700

    Fixed 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import NavBar from '../../common/NavBar';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage: React.FC = () => (
-  <>
-    <NavBar />
-    <h1>404: Page Not Found</h1>
-  </>
+  <div className='flex flex-col max-w-lg mx-auto'>
+    <Link className='mt-4' to='/'>Go back to Calculator</Link>
+    <h1 className='text-2xl mt-4 font-bold mb-4 text-gray-700'>404: Page Not Found</h1>
+  </div>
 );
 
 export default NotFoundPage;
