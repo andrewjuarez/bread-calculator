02df8d78b84dede95e72282579c7aa1e9493e164
andrewjuarez <a777juarez@gmail.com>
2024-02-08 23:51:38 -0800

    Add starter calculator

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,19 @@ import {
 } from 'react-router-dom';
 import './index.css';
 
-import HomePage from './pages/Home';
+import Navbar from './common/NavBar';
+import SourdoughCalculator from './pages/SourdoughCalculator';
+import StarterCalculator from './pages/StarterCalculator';
 import NotFoundPage from './pages/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <div className='flex flex-col w-screen h-screen bg-bgColor'>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          <Route path='/' element={<SourdoughCalculator />} />
+          <Route path='/starter-calculator' element={<StarterCalculator />} />
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
       </div>
